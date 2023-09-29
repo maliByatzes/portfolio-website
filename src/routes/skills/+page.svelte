e104b021253b2a8b03dbb227fa9fea6a93c2dc1a
Malibongwe Gumede <malib2027@gmail.com>
2023-09-29 14:58:51 +0200

    update: added links for technologies

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -12,15 +12,15 @@
   </h3>
   <small class="text-sm font-medium leading-none">% represents the skill level</small>
   <p class="leading-7 font-bold text-xl [&:not(:first-child)]:mt-6">
-    <a href="/" target="_blank" class="hover:underline">golang 78% (main language),</a> <br />
-    <a href="/" target="_blank" class="hover:underline">java 60% (learnt at school),</a> <br />
-    <a href="/" target="_blank" class="hover:underline">postgresql 85% (main sql database),</a> <br />
-    <a href="/" target="_blank" class="hover:underline">redis 68% (main cache database),</a> <br />
-    <a href="/" target="_blank" class="hover:underline">sqlite 80% (learnt at school),</a> <br />
-    <a href="/" target="_blank" class="hover:underline">python 30% (for data structures and algorithms),</a> <br />
-    <a href="/" target="_blank" class="hover:underline">docker 55% (using containers),</a> <br />
+    <a href="https://go.dev/" target="_blank" class="hover:underline">golang 78% (main language),</a> <br />
+    <a href="https://www.java.com/en/" target="_blank" class="hover:underline">java 60% (learnt at school),</a> <br />
+    <a href="https://www.postgresql.org/" target="_blank" class="hover:underline">postgresql 85% (main sql database),</a> <br />
+    <a href="https://redis.io/" target="_blank" class="hover:underline">redis 68% (main cache database),</a> <br />
+    <a href="https://www.sqlite.org/index.html" target="_blank" class="hover:underline">sqlite 80% (learnt at school),</a> <br />
+    <a href="https://www.python.org/" target="_blank" class="hover:underline">python 30% (for data structures and algorithms),</a> <br />
+    <a href="https://www.docker.com/" target="_blank" class="hover:underline">docker 55% (for using containers),</a> <br />
     <a href="/" target="_blank" class="hover:underline">html/css 90% (sometimes for frontend),</a> <br />
-    <a href="/" target="_blank" class="hover:underline">javascript 50% (also sometimes for frontend),</a> <br />
-    <a href="/" target="_blank" class="hover:underline">sveltekit 76% (frontend framework of choice)</a> <br />
+    <a href="https://developer.mozilla.org/en-US/docs/Web/javascript" target="_blank" class="hover:underline">javascript 50% (also sometimes for frontend),</a> <br />
+    <a href="https://kit.svelte.dev/" target="_blank" class="hover:underline">sveltekit 76% (frontend framework of choice)</a> <br />
   </p>
 </main>
